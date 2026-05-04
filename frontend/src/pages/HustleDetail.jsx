5198d9dd4916cd5a6b498b39646068bf9d3787a2
Akash4421 <akashmishra7980@gmail.com>
2026-05-05 02:40:37 +0530

    fix roadmap api url

--- frontend/src/pages/HustleDetail.jsx
+++ frontend/src/pages/HustleDetail.jsx
@@ -41,7 +41,7 @@ const HustleDetail = () => {
 
     const fetchDetails = async () => {
       try {
-        const response = await axios.post('http://side-hustler.onrender.com/api/get-hustle-details', {
+        const response = await axios.post('https://side-hustler.onrender.com/api/get-hustle-details ', {
           title: hustle.title,
           description: hustle.description
         });
