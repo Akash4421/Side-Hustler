8134069fe0e7576c9462aa12459e49dd5e31ef41
Akash4421 <akashmishra7980@gmail.com>
2026-05-05 02:19:14 +0530

    initial commit

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -17,7 +17,7 @@ function Discover() {
     setResults([]);
 
     try {
-      const response = await axios.post('http://side-hustler.onrender.com/api/get-hustles', formData);
+      const response = await axios.post('https://side-hustler.onrender.com/api/get-hustles', formData);
       setResults(response.data.suggestions);
     } catch (err) {
       console.error(err);
